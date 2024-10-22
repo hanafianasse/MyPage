2c13f8482afd37abbb6fcd5b4c721283a7c315b3
Anasse HANAFI <anasse@Anasses-MacBook-Pro.local>
2024-10-22 19:23:53 +0100

    Ajust footer top spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
 
     return (
         <>
-            <div className="bg-[#1e1e1e]">
+            <div className="bg-[#1e1e1e] mt-4">
                 <div className="container mx-auto text-center py-4">
                     © 2024 All rights reserved. | Privacy Policy | Terms of Service
                 </div>
